f8958592f13b9b0da292395881bd6b123e90ae77
Matthias Bathke <matthias-bathke@straightvisions.com>
2020-12-06 14:30:17 +0100

    UPDATE ICON

--- sv_block_separator.php
+++ sv_block_separator.php
@@ -8,9 +8,9 @@
 				->set_css_cache_active()
 				->set_section_title( $this->get_module_title() )
 				->set_section_desc( $this->get_module_desc() )
-				->set_section_type( 'settings' )
 				->set_section_template_path()
 				->set_section_order(5000)
+				->set_section_icon('<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><defs><style>.cls-1{fill-rule:evenodd;}</style></defs><path class="cls-1" d="M24,13H0V12H24Z"/></svg>')
 				->get_root()
 				->add_section( $this );
 		}
