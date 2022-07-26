393574dc0e2de4321d8ad0d6c589a56f7b6040c6
Matthias Bathke <matthias-bathke@straightvisions.com>
2022-07-26 16:25:37 +0200

    fix selector

--- lib/css/config/general.php
+++ lib/css/config/general.php
@@ -1,6 +1,6 @@
 <?php
 	echo $_s->build_css(
-		is_admin() ? '.editor-styles-wrapper .wp-block-separator' : '.wp-block-separator',
+		'.wp-block-separator',
 		array_merge(
 			$module->get_setting('padding')->get_css_data('padding'),
 			$module->get_setting('margin')->get_css_data()
@@ -8,21 +8,21 @@
 	);
 
 	echo $_s->build_css(
-		is_admin() ? '.editor-styles-wrapper .wp-block-separator:not(.is-style-dots)' : '.wp-block-separator:not(.is-style-dots)',
+		'.wp-block-separator:not(.is-style-dots)',
 		array_merge(
 			$module->get_setting('background_color')->get_css_data('background-color')
 		)
 	);
 
 	echo $_s->build_css(
-		is_admin() ? '.editor-styles-wrapper .wp-block-separator:not(.is-style-wide):not(.is-style-dots)' : '.wp-block-separator:not(.is-style-wide):not(.is-style-dots)',
+		'.wp-block-separator:not(.is-style-wide):not(.is-style-dots)',
 		array_merge(
 			$module->get_setting('max_width')->get_css_data('max-width', '', 'px !important')
 		)
 	);
 
 	echo $_s->build_css(
-		is_admin() ? '.editor-styles-wrapper .wp-block-separator.is-style-wide' : '.wp-block-separator.is-style-wide',
+		'.wp-block-separator.is-style-wide',
 		array_merge(
 			$module->get_setting('max_width_style_wide')->get_css_data('max-width', '', 'px !important')
 		)
@@ -32,14 +32,9 @@
 		$colors		= $module->get_module('sv_colors')->get_list();
 
 		foreach($colors as $color){
-			echo is_admin()
-				? '.editor-styles-wrapper .wp-block-separator.has-'.$color['slug'].'-color{'
-				: '.wp-block-separator.has-'.$color['slug'].'-color{'
-			;
-
+			echo '.wp-block-separator.has-'.$color['slug'].'-color{';
 			echo 'color:rgba('.$color['color'].');';
 			echo 'border-color:currentColor;';
-
 			echo '}';
 		}
 	}
